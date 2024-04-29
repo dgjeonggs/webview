42d7bacfeb42892f0c7632395323b33509519e98
DK@GS_ML_Ubuntu <iamjdg11@gmail.com>
2024-04-30 08:31:02 +0900

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+// document.addEventListener('DOMContentLoaded', () => {
+//   main();
+// });
+
+// async function main() {
+const btn1Ele = document.getElementById("button1");
+const btn2Ele = document.getElementById("button2");
+
+btn1Ele.addEventListener('click', () => {
+  window.location.href = 'https://purple-sand-0002e6c00.5.azurestaticapps.net'
+})
+
+btn2Ele.addEventListener('click', () => {
+  window.location.href = 'https://yellow-stone-0767bc100.5.azurestaticapps.net'
+})
+// }
